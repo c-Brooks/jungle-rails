7fb167eceb848258791ff00e2aac241a0e3d8cf1
Corey <c.brooks96@live.ca>
2016-10-13 13:36:50 -0700

    Added validation to signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,12 @@ class ApplicationController < ActionController::Base
 
 # Session logic
   def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-    helper_method :current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 
-    def authorize
-      redirect_to '/login' unless current_user
-    end
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
   has_secure_password
 
     # attr_accessor :password
-    # # EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-    # validates :first_name, :presence => true, :length => { :in => 3..20 }
-    # validates :last_name, :presence => true, :length => { :in => 3..20 }
-    # validates :email, :presence => true #:uniqueness => true #, :format => EMAIL_REGEX
-    # validates :password, :confirmation => true  # password_confirmation attr
-    # validates_length_of :password, :in => 6..20, :on => :create
+     EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+     validates :first_name, :presence => true, :length => { :in => 3..20 }
+     validates :last_name, :presence => true, :length => { :in => 3..20 }
+     validates :email, :presence => true #, :uniqueness => true, :format => EMAIL_REGEX
+     validates :password, :confirmation => true  # password_confirmation attr
+     validates_length_of :password, :in => 6..20, :on => :create
 
 end
