dc0c0bfb6dbd11a72ca7b1abc99670088f9c26d5
Corey <c.brooks96@live.ca>
2016-10-15 19:30:21 -0700

    Added ratings to products index
    TODO: Major refactoring

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,17 +2,19 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    # @product = Product.find
+    # @rating_avg = rating_avg
   end
 
   def show
     @product = Product.find params[:id]
     @reviews = @product.reviews
     @review  = Review.new
-    @rating_avg = rating_avg
+    @rating_avg = rating_avg @product.id
   end
 
   def rating_avg
-    Review.where(product_id: @product.id).average(:rating)
+    Review.where(product_id: @product.id).average(:rating) || 0
   end
 
 end
