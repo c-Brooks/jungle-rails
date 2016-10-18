76b3f0596998539982d49ffe8264337b4b46f320
Corey <c.brooks96@live.ca>
2016-10-18 16:17:52 -0700

    Wrote tests for email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
+    params[:user][:email].downcase!
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ActiveRecord::Base
 
     has_many :reviews
 
-     EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
-     validates :first_name, :presence => true, :length => { :in => 3..20 }
-     validates :last_name, :presence => true, :length => { :in => 3..20 }
-     validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
-     validates :password, :confirmation => true
-     validates_length_of :password, :in => 6..20, :on => :create
+  before_save :downcase_email
 
+   EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+   validates :first_name, :presence => true, :length => { :in => 3..20 }
+   validates :last_name, :presence => true, :length => { :in => 3..20 }
+   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+   validates :password, :confirmation => true
+   validates_length_of :password, :in => 6..20, :on => :create
+
+   def downcase_email
+     self.email.downcase!
+   end
 end
