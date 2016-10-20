f239cc7a4ba6bec1a531af759cbde8de84322e6e
Corey <c.brooks96@live.ca>
2016-10-20 11:19:15 -0700

    Added more info to order page
    Added info on products bought, total order price, and receipt sent to 'email address'

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,4 +6,7 @@ class LineItem < ActiveRecord::Base
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
+  def short_prod_descr
+    product.description.truncate_words(8)
+  end
 end
